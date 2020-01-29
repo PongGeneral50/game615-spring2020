f19e72c97bfe0c440705cc2be4df17ca849f26a0
PongGeneral50 <57685207+PongGeneral50@users.noreply.github.com>
2020-01-29 17:30:55 -0500

    Updates & Excerise2
    Content from Todays Class

---
+++ exercises/Excerise02_Angry_Birds_V01/Assets/_Scripts/PlayerControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public GameObject piggy;
+    public float power = 50f;
+    private Rigidbody2D piggyBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouseInWorld = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+
+        Vector3 direction = mouseInWorld - transform.position;
+
+        float alpha = Mathf.Acos(
+
+        Vector3.Dot(Vector3.right, direction.normalized)) * Mathf.Rad2Deg;
+
+        if (alpha <= 0 && alpha > 0 && direction.y > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, alpha * Mathf.Rad2Deg);
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                piggy.transform.parent = null;
+                piggyBody.gravityScale = 1;
+                piggyBody.AddForce(direction * power);
+            }
+        }
+    }
+}
