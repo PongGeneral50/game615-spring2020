0fce3c9ecdde38f04b0a27e1435d72887b01df93
PongGeneral50 <57685207+PongGeneral50@users.noreply.github.com>
2020-02-12 09:27:56 -0500

    Update on Project 2(Need Assitances on Code for Explosion)
    Note, main issue being the animation and end of explsion script not provided by Unity

--- exercises/revengeofthepiggies-master/Assets/_Scripts/ExplodeBomb.cs
+++ exercises/revengeofthepiggies-master/Assets/_Scripts/ExplodeBomb.cs
@@ -2,22 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExplodeBomb : MonoBehaviour
+public class ExplodeBomb : MonoBehaviour {
 
     public GameObject explosion;
-{
-    // Start is called before the first frame update
     void Update() {
 
         if (Input.GetButtonDown("Jump"))
-    {
-        Instantiate(explosion, Transform.posistion, Quaternion.identity);
-        gameObject.SetActive(false)
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            gameObject.SetActive(false);
+        }
     }
 }
